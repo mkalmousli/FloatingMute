0867bb55998f1fb4d57afcd02e6429b23e90d4ac
mkalmousli <161644468+mkalmousli@users.noreply.github.com>
2025-04-12 18:41:01 +0200

    disable DependencyInfoBlock, for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,15 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    // disable DependencyInfoBlock, for F-Droid.
+    //See: https://gitlab.com/fdroid/admin/-/issues/367
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
 }
 
 dependencies {
